0ecc49914c8279e2a5168e4414168521b562d26c
Julian Castro <julian.castro@binaroo.com>
2022-07-01 10:31:16 +0200

    boilerplate pages and navigation component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { IngredientsComponent } from './pages/ingredients/ingredients.component';
+import { NavigationComponent } from './components/layout/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    IngredientsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
